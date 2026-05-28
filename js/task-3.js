75f5d9e9e765270e98badf2c70d83f390c191b01
Taras <taraszabudsky@gmail.com>
2026-05-28 17:27:29 +0300

    1727

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-input.addEventListener("input", event => {
-  const trimmedValue = event.currentTarget.value.trim();
-
-  output.textContent = trimmedValue || "Anonymous";
-});
